608684c82415ea0ffac59994300b347e762e863b
David Igón <davidbenabarre@platoniq.net>
2023-04-21 16:44:07 +0200

    add first register settings of the plugin

--- server/main.ts
+++ server/main.ts
@@ -1,7 +1,31 @@
 import type { RegisterServerOptions } from '@peertube/peertube-types'
 
-async function register ({ peertubeHelpers }: RegisterServerOptions): Promise<void> {
+async function register ({ peertubeHelpers, registerSetting }: RegisterServerOptions): Promise<void> {
   peertubeHelpers.logger.info('Hello world')
+
+  registerSetting({
+    name: 'goteo-url',
+    label: 'Goteo URL',
+    type: 'input',
+    private: true,
+    default: 'https://api.goteo.org/v1'
+  })
+
+  registerSetting({
+    name: 'goteo-api-user',
+    label: 'Goteo API User',
+    type: 'input',
+    private: true,
+    default: ''
+  })
+
+  registerSetting({
+    name: 'goteo-api-key',
+    label: 'Goteo API Key',
+    type: 'input',
+    private: true,
+    default: ''
+  })
 }
 
 async function unregister (): Promise<void> {}
