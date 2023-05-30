623fe6c656f665dbb60796dd67f101970f0b6d3d
David Igón <davidbenabarre@platoniq.net>
2023-05-30 19:44:29 +0200

    add button in video watch client interface

---
+++ client/video-watch-client-plugin.ts
@@ -0,0 +1,28 @@
+import type { RegisterClientOptions } from "@peertube/peertube-types/client"
+
+async function register ({ registerHook }: RegisterClientOptions ): Promise<void> {
+
+    registerHook({
+        target: 'action:video-watch.video.loaded',
+        handler: () => {
+            const elem = document.createElement('a')
+
+                elem.id = 'btn-fund-goteo-campaign'
+                elem.className = 'btn btn-primary'
+                elem.innerHTML = 'Dona a la campaña'
+                elem.href = 'https://goteo.org/'
+                elem.target = '_blank'
+            
+                const $videoDescriptionList = document.getElementsByTagName('my-video-description');
+
+                // iterate through videodescriptionlist
+                for (const element of $videoDescriptionList) {
+                    element.append(elem)
+                }
+        }
+    })
+}
+
+export {
+    register
+}
