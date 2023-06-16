a0598c59989a0607acfbf9b90d1a5712f8d86d99
David Igón <davidbenabarre@platoniq.net>
2023-06-16 19:48:11 +0200

    show rewards and project button based on video settings

--- client/video-watch-client-plugin.ts
+++ client/video-watch-client-plugin.ts
@@ -1,60 +1,65 @@
 import type { RegisterClientOptions } from '@peertube/peertube-types/client'
+
 import { Marked } from '@ts-stack/markdown'
 
 async function register ({ registerHook, peertubeHelpers }: RegisterClientOptions): Promise<void> {
   registerHook({
     target: 'action:video-watch.video.loaded',
-    handler: () => {
-      const elem: HTMLAnchorElement  = document.createElement('a')
+    handler: (context: any) => {
+      const enableGoteoCampaignValue: boolean = context.video.pluginData['enable-goteo-campaign']
+      const goteoCampaignValue: string = context.video.pluginData['goteo-campaign']
 
-      elem.id = 'btn-fund-goteo-campaign'
-      elem.className = 'btn btn-primary'
-      elem.innerHTML = 'Dona a la campaña'
-      elem.href = 'https://goteo.org/'
-      elem.target = '_blank'
+      if (enableGoteoCampaignValue) {
+        const elem: HTMLAnchorElement  = document.createElement('a')
+        elem.id = 'btn-fund-goteo-campaign'
+        elem.className = 'btn btn-primary'
+        elem.innerHTML = 'Dona a la campaña'
+        elem.href = 'https://goteo.org/project/' + goteoCampaignValue
+        elem.target = '_blank'
 
-      let listOfRewards: HTMLDivElement = document.createElement('div')
-      listOfRewards.classList.add('goteo-project-reward-list')
+        const $videoDescriptionList = document.getElementsByTagName('my-video-description')
 
-      const $videoDescriptionList = document.getElementsByTagName('my-video-description')
+        for (const element of $videoDescriptionList) {
+          element.append(elem)
+        }
 
-      fetch(peertubeHelpers.getBaseRouterRoute() + '/goteo/project/salvaje/rewards')
-        .then(res => res.json())
-        .then((data) => {
-          for (const reward of data) {
+        let listOfRewards: HTMLDivElement = document.createElement('div')
+        listOfRewards.classList.add('goteo-project-reward-list')
 
-            let rewardDiv: HTMLDivElement = document.createElement('div')
-            rewardDiv.classList.add('goteo-project-reward')
+        fetch(peertubeHelpers.getBaseRouterRoute() + '/goteo/project/' + goteoCampaignValue + '/rewards')
+          .then(res => res.json())
+          .then((data) => {
+            for (const reward of data) {
 
-            let rewardName: HTMLHeadingElement = document.createElement('h3')
-            rewardName.classList.add('goteo-project-reward-name')
-            rewardName.innerHTML = reward.name
+              let rewardDiv: HTMLDivElement = document.createElement('div')
+              rewardDiv.classList.add('goteo-project-reward')
 
-            let rewardAmount: HTMLParagraphElement = document.createElement('p')
-            rewardAmount.classList.add('goteo-project-reward-amount')
-            rewardAmount.innerHTML = reward.amount
+              let rewardName: HTMLHeadingElement = document.createElement('h3')
+              rewardName.classList.add('goteo-project-reward-name')
+              rewardName.innerHTML = reward.name
 
-            let rewardDescription: HTMLParagraphElement = document.createElement('p')
-            rewardDescription.classList.add('goteo-project-reward-description')
-            rewardDescription.innerHTML = Marked.parse(reward.description)
+              let rewardAmount: HTMLParagraphElement = document.createElement('p')
+              rewardAmount.classList.add('goteo-project-reward-amount')
+              rewardAmount.innerHTML = reward.amount
 
-            let rewardInvesAnchor: HTMLAnchorElement = document.createElement('a')
-            rewardInvesAnchor.classList.add('btn', 'btn-primary')
-            rewardInvesAnchor.href = 'https://goteo.org/project/salvaje/invest/' + reward.id
-            rewardInvesAnchor.innerHTML = "Donate"
+              let rewardDescription: HTMLParagraphElement = document.createElement('p')
+              rewardDescription.classList.add('goteo-project-reward-description')
+              rewardDescription.innerHTML = Marked.parse(reward.description)
 
-            rewardDiv.append(rewardName)
-            rewardDiv.append(rewardDescription)
-            rewardDiv.append(rewardInvesAnchor)
+              let rewardInvesAnchor: HTMLAnchorElement = document.createElement('a')
+              rewardInvesAnchor.classList.add('btn', 'btn-primary')
+              rewardInvesAnchor.href = 'https://goteo.org/project/' + goteoCampaignValue + '/invest/' + reward.id
+              rewardInvesAnchor.innerHTML = "Donate"
 
-            listOfRewards.append(rewardDiv)
-          }
+              rewardDiv.append(rewardName)
+              rewardDiv.append(rewardDescription)
+              rewardDiv.append(rewardInvesAnchor)
 
-          $videoDescriptionList[0].append(listOfRewards)
-        })
+              listOfRewards.append(rewardDiv)
+            }
 
-      for (const element of $videoDescriptionList) {
-        element.append(elem)
+            $videoDescriptionList[0].append(listOfRewards)
+          })
       }
     }
   })
--- server/main.ts
+++ server/main.ts
@@ -1,29 +1,34 @@
-import type { RegisterServerOptions } from '@peertube/peertube-types'
+import type { RegisterServerOptions, RegisterServerSettingOptions, RegisterServerHookOptions, MVideoFullLight } from '@peertube/peertube-types'
 
-async function register ({ registerSetting, getRouter, settingsManager }: RegisterServerOptions): Promise<void> {
-  registerSetting({
+async function register ({ registerSetting, getRouter, settingsManager, registerHook, storageManager }: RegisterServerOptions): Promise<void> {
+
+  const goteoURLSetting: RegisterServerSettingOptions = {
     name: 'goteo-url',
     label: 'Goteo URL',
     type: 'input',
     private: true,
     default: 'https://api.goteo.org/v1'
-  })
+  }
 
-  registerSetting({
+  const goteoAPIUserSetting: RegisterServerSettingOptions = {
     name: 'goteo-api-user',
     label: 'Goteo API User',
     type: 'input',
     private: true,
     default: ''
-  })
+  }
 
-  registerSetting({
+  const goteoAPIKeySetting: RegisterServerSettingOptions = {
     name: 'goteo-api-key',
     label: 'Goteo API Key',
-    type: 'input',
+    type: 'input-password',
     private: true,
     default: ''
-  })
+  }
+
+  registerSetting(goteoURLSetting)
+  registerSetting(goteoAPIUserSetting)
+  registerSetting(goteoAPIKeySetting)
 
   const router = getRouter()
   router.get('/login', async (req, res) => {
@@ -57,6 +62,47 @@ async function register ({ registerSetting, getRouter, settingsManager }: Regist
     const data = await response.json()
     res.send(data.rewards)
   })
+
+  const updateVideoHook: RegisterServerHookOptions = {
+    target: 'action:api.video.updated',
+    handler: async (params: any) => {
+      const video = params.video as MVideoFullLight
+      const body = params.body
+ 
+      if (!body.pluginData) return
+
+      const enableGoteoOptionValue = body.pluginData['enable-goteo-campaign']
+      if (!enableGoteoOptionValue) return
+
+      const goteoCampaignOptionValue = body.pluginData['goteo-campaign']
+      if (!goteoCampaignOptionValue) return
+
+      storageManager.storeData('enable-goteo-campaign-' + video.id, enableGoteoOptionValue)
+      storageManager.storeData('goteo-campaign-' + video.id, goteoCampaignOptionValue)
+    }
+  }
+
+  registerHook(updateVideoHook)
+
+  const videoGetResultHook: RegisterServerHookOptions = {
+    target: 'filter:api.video.get.result',
+    handler: async (video: any) => {
+      if (!video) return video
+      if (!video.pluginData) video.pluginData = {}
+
+      const enableGoteoOptionValue = await storageManager.getData('enable-goteo-campaign-' + video.id)
+      const goteoCampaignOptionValue = await storageManager.getData('goteo-campaign-' + video.id)
+
+      console.log('enableGoteoOptionValue', enableGoteoOptionValue)
+      console.log('goteoCampaignOptionValue', goteoCampaignOptionValue)
+
+      video.pluginData['enable-goteo-campaign'] = enableGoteoOptionValue
+      video.pluginData['goteo-campaign'] = goteoCampaignOptionValue
+
+      return video
+    }
+  }
+  registerHook(videoGetResultHook)
 }
 
 async function unregister (): Promise<void> {}
