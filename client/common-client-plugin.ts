36b9d32411dc11012f0b79cccd84537e3952c3e9
David Igón <david.igon@riseup.net>
2024-06-27 11:49:56 +0200

    upgrade peertube to 6.1

--- client/common-client-plugin.ts
+++ client/common-client-plugin.ts
@@ -1,34 +1,46 @@
-import type { RegisterClientOptions } from '@peertube/peertube-types/client'
-import type { RegisterClientFormFieldOptions, RegisterClientVideoFieldOptions } from '@peertube/peertube-types'
+import type { RegisterClientOptions } from "@peertube/peertube-types/client";
+import type {
+  RegisterClientFormFieldOptions,
+  RegisterClientVideoFieldOptions,
+} from "@peertube/peertube-types";
 
-async function register ({ registerVideoField }: RegisterClientOptions): Promise<void> {
+async function register(
+  { registerVideoField }: RegisterClientOptions,
+): Promise<void> {
   const enableGoteoOptions: RegisterClientFormFieldOptions = {
-    name: 'enable-goteo-campaign',
-    label: 'Enable Goteo Campaign',
-    descriptionHTML: 'This options enables the connection with a campaign in the CrowdFunding platform Goteo.org',
-    type: 'input-checkbox',
-    default: false
-  }
+    name: "enable-goteo-campaign",
+    label: "Enable Goteo Campaign",
+    descriptionHTML:
+      "This options enables the connection with a campaign in the CrowdFunding platform Goteo.org",
+    type: "input-checkbox",
+    default: false,
+  };
 
   const campaignGoteoOptions: RegisterClientFormFieldOptions = {
-    name: 'goteo-campaign',
-    label: 'Goteo Campaign ID',
-    descriptionHTML: 'This is the ID of the campaign in Goteo.org',
-    type: 'input',
-    default: ''
-  }
+    name: "goteo-campaign",
+    label: "Goteo Campaign ID",
+    descriptionHTML: "This is the ID of the campaign in Goteo.org",
+    type: "input",
+    default: "",
+  };
 
-  for (const type of ['upload', 'import-url', 'import-torrent', 'update', 'go-live']) {
+  for (
+    const type of [
+      "upload",
+      "import-url",
+      "import-torrent",
+      "update",
+      "go-live",
+    ]
+  ) {
     const videoFormOptions: RegisterClientVideoFieldOptions = {
-      type: type as RegisterClientVideoFieldOptions['type'],
-      tab: 'main'
-    }
+      type: type as RegisterClientVideoFieldOptions["type"],
+      tab: "main",
+    };
 
-    registerVideoField(enableGoteoOptions, videoFormOptions)
-    registerVideoField(campaignGoteoOptions, videoFormOptions)
+    registerVideoField(enableGoteoOptions, videoFormOptions);
+    registerVideoField(campaignGoteoOptions, videoFormOptions);
   }
 }
 
-export {
-  register
-}
+export { register };
